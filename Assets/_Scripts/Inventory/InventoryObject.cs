3dc9a16e58b202491915bd1e93c2b24b0984dbb6
JaredMoore-Acer <JaredMoore-Acer@LAPTOP-5GT1TFRD>
2020-08-26 20:27:22 +0930

    Created campfire prefab. Implemented interactibles scripts for rock/stick pickups. Refined inventory layout. Environmental/world tweaks.

---
+++ Assets/_Scripts/Inventory/InventoryObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour
+{
+    public Item item;
+
+    public void Pickup()
+    {
+        Inventory.instance.Pickup(item);
+        gameObject.SetActive(false);
+    }
+}
