5d2372fb837f8366b10c26815c242ca9c69b41e2
JaredMoore-Acer <JaredMoore-Acer@LAPTOP-5GT1TFRD>
2020-10-14 23:04:26 +1030

    Weapon manager & handler script work. Begun player attacks scripts.

---
+++ Assets/_Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private WeaponManager weapon_Manager;
+
+    public float fireRate = 15f;
+    private float nextTimeToFire;
+    public float damage = 20f;
+
+    void Awake()
+    {
+        weapon_Manager = GetComponent<WeaponManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WeaponShoot();
+    }
+
+    void WeaponShoot()
+    {
+        // If we have assault rifle (Probably not required in final version)
+        if (weapon_Manager.GetCurrentSelectedWeapon().fireType == WeaponFireType.MULTIPLE)
+        {
+            // If we press and hold left mouse click AND
+            // if Time is greater than the nextTimeToFire
+            if (Input.GetMouseButton(0) && Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+
+                weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                // BulletFired();
+            }
+        }
+        // If we have a weapon that shoots once (Axe/Revolver)
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Handle axe
+                if (weapon_Manager.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG)
+                {
+                    weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+                // Handle shoot
+                if (weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
+                {
+                    weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                    // BulletFired();
+                }
+            // If input gets mouse button 0
+            }
+        // else
+        }
+    }
+}
